4f2cd14244e37f74c342d562cf441bcb5b2c4466
João D <joao.luis.domingos@gmail.com>
2024-06-14 16:34:36 +0100

    chore: simplify

--- src/tron.ts
+++ src/tron.ts
@@ -1,5 +1,4 @@
 import { TronWeb } from 'tronweb';
-import { HttpProvider } from 'tronweb/lib/esm/lib/providers';
 import {
   WalletOptions,
   isLedgerOptions,
@@ -20,14 +19,10 @@ export const getTronWallet = async (options: WalletOptions) => {
     walletDerivationPaths[walletType].tron(index),
   ).privateKey;
 
-  const fullNode = new HttpProvider(TRON_HOST);
-  const solidityNode = new HttpProvider(TRON_HOST);
-  const eventServer = new HttpProvider(TRON_HOST);
-
   const tronWallet = new TronWeb(
-    fullNode,
-    solidityNode,
-    eventServer,
+    TRON_HOST,
+    TRON_HOST,
+    TRON_HOST,
     privateKey.substring(2),
   );
 
