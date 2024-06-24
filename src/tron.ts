447e2e3c6d7c88464bcc3d47586fb1abc6ed3652
João <joao.luis.domingos@gmail.com>
2024-06-24 10:24:42 +0100

    Update tronweb (#8)
    * chore: update tronweb
    
    * chore: bump version
    
    * chore: improve tron signer
    
    * chore: simplify
    
    * chore: bump

--- src/tron.ts
+++ src/tron.ts
@@ -1,8 +1,8 @@
-import * as TronWeb from 'tronweb';
+import { TronWeb } from 'tronweb';
 import {
+  WalletOptions,
   isLedgerOptions,
   walletDerivationPaths,
-  WalletOptions,
 } from './constants';
 
 const TRON_HOST = 'https://api.trongrid.io';
@@ -19,15 +19,10 @@ export const getTronWallet = async (options: WalletOptions) => {
     walletDerivationPaths[walletType].tron(index),
   ).privateKey;
 
-  const HttpProvider = TronWeb.providers.HttpProvider;
-  const fullNode = new HttpProvider(TRON_HOST);
-  const solidityNode = new HttpProvider(TRON_HOST);
-  const eventServer = new HttpProvider(TRON_HOST);
-
   const tronWallet = new TronWeb(
-    fullNode,
-    solidityNode,
-    eventServer,
+    TRON_HOST,
+    TRON_HOST,
+    TRON_HOST,
     privateKey.substring(2),
   );
 
